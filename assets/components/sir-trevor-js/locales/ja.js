6b3459c2ee77277c58873560e949930c7ec0a1d3
1shiharaT <akeome1369@gmail.com>
2015-08-15 19:41:08 +0900

    必要なファイルを管理下に

---
+++ assets/components/sir-trevor-js/locales/ja.js
@@ -0,0 +1,57 @@
+SirTrevor.Locales.ja = {
+  general: {
+    'delete': '削除しますか?',
+    'drop': 'ここに __block__ をドラッグ',
+    'paste': 'またはここにリンクをコピー',
+    'upload': '...またはファイルを選択します',
+    'close': '閉じる',
+    'position': 'ポジション',
+    'wait': 'お待ちください...',
+    'link': 'リンクを紹介'
+  },
+  errors: {
+    'title': "次のエラーが発生しました。:",
+    'validation_fail': "__type__ ブロックが無効です",
+    'block_empty': "__name__ は必ず値が必要です。",
+    'type_missing': "あなたが __type__ タイプのブロックを持っている必要があります ",
+    'required_type_empty': "__type__ は必ず値の入力が必要ですが、空になっています。",
+    'load_fail': "コンテンツの内容を読み込み中に問題が発生しました"
+  },
+  blocks: {
+    text: {
+      'title': "テキスト"
+    },
+    list: {
+      'title': "リスト"
+    },
+    quote: {
+      'title': "引用",
+      'credit_field': "著者"
+    },
+    image: {
+      'title': "画像",
+      'upload_error': "画像のアップロードに失敗しました"
+    },
+    video: {
+      'title': "動画"
+    },
+    tweet: {
+      'title': "ツイート",
+      'fetch_error': "ツイートを読み込むことが出来ませんでした。"
+    },
+    embedly: {
+      'title': "埋め込み",
+      'fetch_error': "あなたのビデオを取得中に問題が発生しました",
+      'key_missing': "あなたは、関連するAPIキーを持っている必要があります"
+    },
+    heading: {
+      'title': '見出し'
+    },
+    tinymce: {
+      'title' : 'エディタ'
+    },
+    columns: {
+      'title' : 'カラム'
+    }
+  }
+};
